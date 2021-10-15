f18ee14ff84cf81d1bd4c62f82fda7364c6785dd
김병수 <ethan@perfitt.io>
2021-10-16 00:31:55 +0900

    init

---
+++ bento_packer.py
@@ -0,0 +1,12 @@
+from models.train import clf
+
+from bento_service import IrisClassifier
+
+# Create a iris classifier service instance
+iris_classifier_service = IrisClassifier()
+
+# Pack the newly trained model artifact
+iris_classifier_service.pack('model', clf)
+
+# Save the prediction service to disk for model serving
+saved_path = iris_classifier_service.save()
